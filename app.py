74d478d36bd45449d04d60c6942bd6c480f136dc
Tim Lucas <tllucas1s@gmail.com>
2018-01-30 12:47:22 -0600

    Updated db password and username.

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True      # displays runtime errors in the browser, too
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tllucas1s_car-log:SXJ#Rs]ATabL@localhost:3306/tllucas1s_car-log'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://tllucas1_car-log:SXJ#Rs]ATabL@localhost:3306/tllucas1_car-log'
 app.config['SQLALCHEMY_ECHO'] = True
 
 db = SQLAlchemy(app)
